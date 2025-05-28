084832e7bd457d24563b72b77f095bd8d4f1f704
longxingren <973506608@qq.com>
2025-05-28 22:46:45 +0800

    feat: added logo svg; added vite svgr plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Logo from '@/assets/icons/snippix.svg?react';
 import Editor from '@/components/Editor';
+import LanguageSelection from '@/components/LanguageSelection';
 import ResizableBox from '@/components/ResizableBox';
+import ThemeSelection from '@/components/ThemeSelection';
+import { langugageList } from '@/configs/langugage-list';
+import { themeList } from '@/configs/theme-list';
 import { useCallback, useState } from 'react';
-import LanguageSelection from './components/LanguageSelection';
-import ThemeSelection from './components/ThemeSelection';
-import { langugageList } from './configs/langugage-list';
-import { themeList } from './configs/theme-list';
 
 function App() {
   const [language, setLauguage] = useState<string>('javascript');
@@ -19,13 +20,23 @@ function App() {
   }, []);
 
   return (
-    <>
-      <LanguageSelection list={langugageList} onChange={handleLanguageChange} />
-      <ThemeSelection list={themeList} onChange={handleThemeChange} />
-      <ResizableBox>
-        <Editor language={language} theme={theme} />
-      </ResizableBox>
-    </>
+    <div className="flex flex-col h-full bg-[#1e1e1e]">
+      <header className="h-[60px] border-b border-[#374151] pl-[40px]">
+        <div className="inline-block w-[55px] h-[55px]">
+          <Logo width="100%" fill="white" />
+        </div>
+        <LanguageSelection list={langugageList} onChange={handleLanguageChange} />
+        <ThemeSelection list={themeList} onChange={handleThemeChange} />
+      </header>
+      <main className="flex flex-1">
+        <div className="flex-1">
+          <ResizableBox>
+            <Editor language={language} theme={theme} />
+          </ResizableBox>
+        </div>
+        <aside className="w-[300px] border-l border-[#374151]"></aside>
+      </main>
+    </div>
   );
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react';
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
